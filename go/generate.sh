0e6b8f596e36e942d0333765d2792b4b644149b9
Tom Haskell <tom.haskell@vanti.co.uk>
2019-09-09 12:19:06 +0100

    Imported & generated the Info API from old sc repo
    This allows exposing information about an endpoint related to devices

--- go/generate.sh
+++ go/generate.sh
@@ -4,6 +4,7 @@ set -e
 
 protoc -I ../protobuf --go_out=plugins=grpc,paths=source_relative:./ ../protobuf/types/*.proto
 protoc -I ../protobuf --go_out=plugins=grpc,paths=source_relative:./ ../protobuf/device/traits/*.proto
+protoc -I ../protobuf --go_out=plugins=grpc,paths=source_relative:./ ../protobuf/device/info/*.proto
 
 go build ./...
 go test ./...
