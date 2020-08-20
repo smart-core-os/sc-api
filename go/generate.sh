f06851b05f0249320f47ede2f843011ed2d144bf
Ben Jacobs <ben.jacobs@vanti.co.uk>
2020-08-20 14:36:05 +0100

    feat(go): add time period and booking trait generated code

--- go/generate.sh
+++ go/generate.sh
@@ -3,6 +3,7 @@
 set -e
 
 protoc -I ../protobuf --go_out=paths=source_relative:./ ../protobuf/types/*.proto --go-grpc_out=paths=source_relative:./ ../protobuf/types/*.proto
+protoc -I ../protobuf --go_out=paths=source_relative:./ ../protobuf/types/time/*.proto --go-grpc_out=paths=source_relative:./ ../protobuf/types/time/*.proto
 
 protoc -I ../protobuf --go_out=paths=source_relative:./ ../protobuf/device/traits/*.proto --go-grpc_out=paths=source_relative:./ ../protobuf/device/traits/*.proto
 
