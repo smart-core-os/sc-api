cf57971c4f25f0c416855e160fd0c77f665471f3
Tom Haskell <tom.haskell@vanti.co.uk>
2022-05-31 10:26:42 +0100

    dev: add docs publish script for pushing to smart-core-os docs repo

--- docs/publish.sh
+++ docs/publish.sh
@@ -2,3 +2,27 @@
 
 set -e # if a command fails it stops the execution
 
+echo "[+] Publishing API docs"
+
+if [ $1 = "prepare" ]; then
+  echo "- Preparing"
+  echo "    git clone git@github.com:smart-core-os/smart-core-os.github.io.git ./target"
+  git clone git@github.com:smart-core-os/smart-core-os.github.io.git ./target
+  echo "    copying docs files"
+  cp -R ./api/* ./target/src/api
+  echo "    committing generated docs to repo"
+  pushd target
+  git add src/api --force
+  git commit -m "chore: generated API docs v$2"
+  popd
+fi
+
+if [ $1 = "perform" ]; then
+  echo "- Peforming"
+  pushd target
+  echo "    pushing to docs repo"
+  git push origin main
+  echo "    tidying up"
+  popd
+  rm -rf ./target
+fi
